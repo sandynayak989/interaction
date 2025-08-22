e99a89fccff5905f69afe85863d3b91579745f4b
sandynayak989 <sandynayak989@gmail.com>
2025-08-22 18:35:54 +0530

    fixed bugs in interaction

--- interaction.py
+++ interaction.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, send_file, flash
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory, send_file, flash
 import pandas as pd
 import os
 import hashlib
